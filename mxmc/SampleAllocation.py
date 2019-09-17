cf0794d2d05723ae2a4d72bece29fe17cffd1c4a
William P. Leser <william.leser@nasa.gov>
2019-09-17 15:44:05 -0400

    expanded allocation feature added

--- mxmc/SampleAllocation.py
+++ mxmc/SampleAllocation.py
@@ -1,7 +1,36 @@
 import pandas as pd
 import numpy as np
 
+
 class SampleAllocation:
     def __init__(self, compressed_allocation):
-        self.compressed_allocation = compressed_allocation
+        self.compressed_allocation = compressed_allocation.tolist()
+        self.num_models = self._calculate_num_models()
+        self.expanded_allocation = self._expand_allocation()
 
+    def get_number_of_samples_per_model(self):
+        samples_per_model = np.zeros(self.num_models)
+
+    def _expand_allocation(self):
+        expanded_allocation_data_frames = []
+
+        for i in range(len(self.compressed_allocation)):
+            row = self.compressed_allocation[i].copy()
+            multiplier = row.pop(0)
+            expanded_allocation_data_frames.append(pd.DataFrame(columns=self._get_column_names(),
+                                                                data=[row] * multiplier))
+        expanded_dataframe = pd.concat(expanded_allocation_data_frames, ignore_index=True)
+        return expanded_dataframe
+
+    def _calculate_num_models(self):
+        return int(1 + (np.shape(self.compressed_allocation)[1] - 2) / 2)
+
+    def _get_column_names(self):
+        column_names = []
+        for i in range(self.num_models):
+            if i == 0:
+                column_names.append(str(i))
+            else:
+                column_names.append(str(i) + '_1')
+                column_names.append(str(i) + '_2')
+        return column_names
--- tests/unit/test_sample_allocation.py
+++ tests/unit/test_sample_allocation.py
@@ -1,7 +1,70 @@
 import pytest
+import pandas as pd
+import numpy as np
+
 from mxmc.SampleAllocation import SampleAllocation
 
 
-def test_sample_allocation_init():
-    sample_allocation_object = SampleAllocation([])
-    assert sample_allocation_object.compressed_allocation == []
+# def test_init():
+#    sample_allocation_object = SampleAllocation(np.array([[]]))
+#    assert sample_allocation_object.compressed_allocation == [[]]
+
+
+@pytest.fixture
+def compressed_allocation():
+    return np.array([[1, 1, 1, 1, 0, 0], [5, 0, 1, 1, 1, 1], [10, 0, 0, 0, 1, 1]])
+
+
+@pytest.fixture
+def expanded_allocation():
+    return [[1, 1, 1, 0, 0],
+            [0, 1, 1, 1, 1],
+            [0, 1, 1, 1, 1],
+            [0, 1, 1, 1, 1],
+            [0, 1, 1, 1, 1],
+            [0, 1, 1, 1, 1],
+            [0, 0, 0, 1, 1],
+            [0, 0, 0, 1, 1],
+            [0, 0, 0, 1, 1],
+            [0, 0, 0, 1, 1],
+            [0, 0, 0, 1, 1],
+            [0, 0, 0, 1, 1],
+            [0, 0, 0, 1, 1],
+            [0, 0, 0, 1, 1],
+            [0, 0, 0, 1, 1],
+            [0, 0, 0, 1, 1]]
+
+
+@pytest.fixture
+def expanded_allocation_dataframe(expanded_allocation):
+    return pd.DataFrame(columns=['0', '1_1', '1_2', '2_1', '2_2'], data=expanded_allocation)
+
+
+@pytest.fixture
+def sample_allocation(compressed_allocation):
+    return SampleAllocation(compressed_allocation)
+
+
+def test_compressed_allocation(sample_allocation, compressed_allocation):
+    assert sample_allocation.compressed_allocation == compressed_allocation.tolist()
+
+
+def test_num_models(sample_allocation):
+    assert sample_allocation.num_models == 3
+
+
+def test_get_column_names(sample_allocation):
+    assert sample_allocation._get_column_names() == ['0', '1_1', '1_2', '2_1', '2_2']
+
+
+def test_one_model_num_models():
+    test_allocation = SampleAllocation(np.array([[10, 1]]))
+    assert test_allocation.num_models == 1
+
+
+def test_expand_allocation(sample_allocation, expanded_allocation_dataframe):
+    assert pd.DataFrame.equals(sample_allocation.expanded_allocation, expanded_allocation_dataframe)
+
+
+def test_get_number_of_samples_per_model(sample_allocation):
+    assert sample_allocation.get_number_of_samples_per_model() == np.array([1, 6, 15])
