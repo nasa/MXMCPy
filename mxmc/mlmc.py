fdb4d8441e698405dfbc44bcd9a63f9f900a4d22
jwarner308 <james.e.warner@nasa.gov>
2019-09-25 18:47:56 -0400

    Simple tests added / passed for mlmc

--- mxmc/mlmc.py
+++ mxmc/mlmc.py
@@ -6,7 +6,11 @@ from .optimizer import OptimizationResult, Optimizer
 
 class MLMC(Optimizer):
     def __init__(self, model_costs, covariance=None, mlmc_variances=None):
-        return
+
+        if type(mlmc_variances) != np.ndarray:
+            raise ValueError("MLMC variances must be numpy array")
+        if len(model_costs) != len(mlmc_variances):
+            raise ValueError("Model costs & variances must have same length!")
 
 
     def optimize(self, target_cost):
@@ -16,4 +20,5 @@ class MLMC(Optimizer):
         allocation = np.array([[0]])
         result = OptimizationResult(actual_cost, estimator_variance, allocation)
         return result
+    
     
--- tests/unit/test_mlmc.py
+++ tests/unit/test_mlmc.py
@@ -15,3 +15,27 @@ def test_optimizer_returns_tuple_with(mlmc_optimizer):
     opt_result = mlmc_optimizer.optimize(target_cost=10)
     for member in ["cost", "variance", "sample_array"]:
         assert member in dir(opt_result)
+
+def test_cost_variances_mismatch_raises_error():
+    
+    costs = np.array([1,2,3])
+    variances = np.array([1,2])
+    with pytest.raises(ValueError):
+        mlmc = MLMC(model_costs=costs, mlmc_variances=variances)
+
+@pytest.mark.parametrize("variances", [1, None, "varz"])
+def test_mlmc_variance_is_right_type_or_raises_error(variances):
+
+    with pytest.raises(ValueError):
+        mlmc = MLMC(model_costs=np.array([1]), mlmc_variances=variances)
+
+
+@pytest.mark.parametrize("target_cost", [-1, 0.5, 0])
+def test_target_cost_too_low_to_run_models(mlmc_optimizer, target_cost):
+
+    opt_result = mlmc_optimizer.optimize(target_cost)
+    assert opt_result.cost == 0
+    assert np.isinf(opt_result.variance)
+    np.testing.assert_array_almost_equal(opt_result.sample_array,
+                                         np.array([[0, 1, 1, 1]], dtype=int))
+
