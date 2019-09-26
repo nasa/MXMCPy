807d9e7dadd959e6189f931923836600d48605f5
Geoffrey Bomarito <geoffrey.f.bomarito@nasa.gov>
2019-09-25 18:07:00 -0600

    cleaned up and simplified mfmc

--- mxmc/optimizer.py
+++ mxmc/optimizer.py
@@ -10,6 +10,7 @@ OptimizationResult = namedtuple('OptimizationResult',
 class Optimizer(metaclass=ABCMeta):
     def __init__(self, model_costs, covariance=None):
         self._model_costs = model_costs
+        self._covariance = covariance
         self._num_models = len(self._model_costs)
         if covariance is not None:
             self._validation(covariance)
@@ -23,3 +24,14 @@ class Optimizer(metaclass=ABCMeta):
     @abstractmethod
     def optimize(self, target_cost):
         raise NotImplementedError
+
+    def subset(self, model_indices):
+        subset_costs = np.copy(self._model_costs[model_indices])
+        subset_covariance = \
+            np.copy(self._covariance[model_indices][:, model_indices])
+        return self.__class__(subset_costs, subset_covariance)
+
+    def get_invalid_result(self):
+        allocation = np.ones((1, 2 * self._num_models))
+        allocation[0, 0] = 0
+        return OptimizationResult(0, np.inf, allocation)
