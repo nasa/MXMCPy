c65e11b609ca0f88f9120d5b08b6b17529fe9b9a
Geoffrey Bomarito <geoffrey.f.bomarito@nasa.gov>
2019-10-02 16:26:55 -0600

    small refactor of estimator

--- mxmc/estimator.py
+++ mxmc/estimator.py
@@ -6,24 +6,20 @@ class Estimator:
     def __init__(self, allocation, covariance):
         self._allocation = allocation
         self._covariance = covariance
+        self._num_models = self._allocation.num_models
+        self._num_samples_per_model = \
+            self._allocation.get_number_of_samples_per_model()
         if covariance is not None:
             self._validation(covariance)
 
     def _validation(self, covariance):
-        if len(covariance) != self._allocation.num_models:
+        if len(covariance) != self._num_models:
             raise ValueError("Covariance and allocation dimensions must match")
         if not np.allclose(covariance.transpose(), covariance):
             raise ValueError("Covariance array must be symmetric")
 
     def get_estimate(self, model_outputs):
-        if len(model_outputs) != self._allocation.num_models:
-            raise ValueError("Number of models in model output did not match "
-                             "the number in sample allocation")
-        model_samples = self._allocation.get_number_of_samples_per_model()
-        for outputs, num_samps in zip(model_outputs, model_samples):
-            if len(outputs) != num_samps:
-                raise ValueError("Number of outputs per model does not match "
-                                 "the sample allocation")
+        self._validate_model_outputs(model_outputs)
 
         k0 = self._allocation.get_k0_matrix()
         k = self._allocation.get_k_matrix()
@@ -41,6 +37,16 @@ class Estimator:
                 Q += alpha[i-1] * (np.mean(Q_i1) - np.mean(Q_i2))
 
         return Q
+
+    def _validate_model_outputs(self, model_outputs):
+        if len(model_outputs) != self._num_models:
+            raise ValueError("Number of models in model output did not match "
+                             "the number in sample allocation")
+        for outputs, num_samps in zip(model_outputs,
+                                      self._num_samples_per_model):
+            if len(outputs) != num_samps:
+                raise ValueError("Number of outputs per model does not match "
+                                 "the sample allocation")
 
 
 
