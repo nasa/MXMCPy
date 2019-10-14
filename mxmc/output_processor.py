1bbaa64f71998f6313570838c71fa69d7d62f963
Patrick E. Leser <pleser@k-li1-ib0.ccf-beowulf.larc.nasa.gov>
2019-10-14 12:04:23 -0400

    can now compute covariance matrix from a list of list of model outputs and a sample allocation object

--- mxmc/output_processor.py
+++ mxmc/output_processor.py
@@ -1,21 +1,32 @@
+from functools import reduce
 import numpy as np
 import pandas as pd
 
 class OutputProcessor():
 
-    def compute_covariance_matrix(model_outputs):
+    def __init__(self):
+        pass
+
+    def compute_covariance_matrix(self, model_outputs, sample_allocation=None):
+        '''
+        '''
         if len(model_outputs) == 1 and model_outputs[0].size == 1:
             return np.array([np.nan])
 
         if len(model_outputs) == 0:
             return np.array([])
 
-        if len({model_output.size for model_output in model_outputs}) == 1:
-            return np.cov(model_outputs)
+        if sample_allocation is None:
+            if len({model_output.size for model_output in model_outputs}) == 1:
+                return np.cov(model_outputs)
 
-        output_df = pd.DataFrame(model_outputs)
+            output_df = pd.DataFrame(model_outputs)
 
-        cov = np.diag([np.var(row) for _, row in output_df.iterrows()])
+        else:
+            output_df = self._assemble_output_df(model_outputs,
+                                                 sample_allocation)
+
+        cov = np.diag([np.var(row, ddof=1) for _, row in output_df.iterrows()])
 
         num_models = len(model_outputs)
         for i in range(num_models - 1):
@@ -29,3 +40,17 @@ class OutputProcessor():
                 cov[i, j] = element_cov
                 cov[j, i] = element_cov
         return cov
+
+    @staticmethod
+    def _assemble_output_df(model_outputs, sample_alloc):
+
+        sub_dfs = []
+        for model_index, outputs in enumerate(model_outputs):
+            alloc = sample_alloc.get_sample_indices_for_model(model_index)
+            sub_dfs.append(pd.DataFrame({model_index: outputs,
+                                         'alloc_indices': alloc}))
+
+        merge_func = lambda x, y: x.merge(y, how='outer')
+        output_df = reduce(merge_func, sub_dfs).set_index('alloc_indices')
+        output_df.sort_index(inplace=True)
+        return output_df.T
--- tests/unit/test_output_processor.py
+++ tests/unit/test_output_processor.py
@@ -2,30 +2,56 @@ import pytest
 import numpy as np
 from mxmc.output_processor import OutputProcessor
 
+class SampleAllocationStub:
 
-def test_compute_covariance_matrix_returns_nan_array_given_no_outputs():
-    covariance = OutputProcessor.compute_covariance_matrix([])
+    def __init__(self, model_indices):
+        self.model_indices = model_indices
+
+    def get_sample_indices_for_model(self, model):
+        return self.model_indices[model]
+
+
+@pytest.fixture
+def output_processor():
+    return OutputProcessor()
+
+
+def test_compute_cov_matrix_return_nan_array_if_no_outputs(output_processor):
+    covariance = output_processor.compute_covariance_matrix([])
     assert all(np.isnan(covariance))
 
 
-def test_compute_covariance_matrix_one_number_is_nan_array():
-    covariance = OutputProcessor.compute_covariance_matrix([np.array(1)])
+def test_compute_covariance_matrix_one_number_is_nan_array(output_processor):
+    covariance = output_processor.compute_covariance_matrix([np.array(1)])
     assert all(np.isnan(covariance))
 
 
-def test_compute_covariance_matrix_two_samples():
+def test_compute_covariance_matrix_two_samples(output_processor):
     model_outputs = [np.array([1, 2])]
-    covariance = OutputProcessor.compute_covariance_matrix(model_outputs)
+    covariance = output_processor.compute_covariance_matrix(model_outputs)
     assert covariance == np.array([0.5])
 
 
-def test_compute_covariance_matrix_two_model_zero_variance():
+def test_compute_covariance_matrix_two_model_zero_variance(output_processor):
     model_outputs = [np.array([1, 1]), np.array([1, 1])]
-    covariance = OutputProcessor.compute_covariance_matrix(model_outputs)
+    covariance = output_processor.compute_covariance_matrix(model_outputs)
     np.testing.assert_array_equal(covariance, np.zeros((2, 2)))
 
 
-def test_compute_covariance_matrix_two_model_different_sizes():
+def test_compute_covariance_matrix_two_model_different_sizes(output_processor):
     model_outputs = [np.array([1, 1]), np.array([1, 1, 1]), np.array([1, 1])]
-    covariance = OutputProcessor.compute_covariance_matrix(model_outputs)
+    covariance = output_processor.compute_covariance_matrix(model_outputs)
     np.testing.assert_array_equal(covariance, np.zeros((3, 3)))
+
+
+def test_compute_covariance_matrix_with_sample_allocation(output_processor):
+    model_indices = {0: [1, 2, 4], 1: [2, 3, 4], 2: [0, 1, 2, 3]}
+    sample_alloc = SampleAllocationStub(model_indices)
+    model_outputs = [np.array([1, 2, 2.5]), np.array([1., 2., 3.]),
+                     np.array([1., -0.5, 2., 1.5])]
+
+    covariance = output_processor.compute_covariance_matrix(model_outputs,
+                                                            sample_alloc)
+
+    expected = np.array([[7/12., .5, 1.25], [.5, 1., -.25], [1.25, -.25, 7/6.]])
+    np.testing.assert_array_almost_equal(covariance, expected)
