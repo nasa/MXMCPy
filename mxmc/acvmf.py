21a8716a3d5b438449e19f7e46037302dc573c7f
jwarner308 <james.e.warner@nasa.gov>
2019-11-08 12:46:25 -0500

    ACVMF refactor

--- mxmc/acvmf.py
+++ mxmc/acvmf.py
@@ -12,6 +12,8 @@ class ACVMF(OptimizerBase):
     def optimize(self, target_cost):
         if target_cost < np.sum(self._model_costs):
             return self.get_invalid_result()
+        if self._num_models == 1:
+            return self._get_monte_carlo_opt_result(target_cost)
 
         sample_nums = self._solve_opt_problem(target_cost)
         sample_nums = np.floor(sample_nums)
@@ -21,10 +23,16 @@ class ACVMF(OptimizerBase):
 
         return OptimizationResult(cost, variance, allocation)
 
+    def _get_monte_carlo_opt_result(self, target_cost):
+
+        sample_nums = np.floor(np.array([target_cost/self._model_costs[0]]))
+        variance = self._covariance[0,0]/sample_nums[0]
+        cost = self._get_total_cost(sample_nums)
+        allocation = self._make_allocation(sample_nums)
+        return OptimizationResult(cost, variance, allocation)
+
     def _solve_opt_problem(self, target_cost):
         
-        if self._num_models == 1:
-            return np.array([target_cost/self._model_costs[0]])
         initial_guess = np.ones(self._num_models)
         constraints = [self._get_cost_constraint(target_cost)]
         bounds = [(1, np.inf) for i in range(self._num_models)]
@@ -41,8 +49,9 @@ class ACVMF(OptimizerBase):
 
         total_sample_nums = np.zeros(len(sample_nums))
         total_sample_nums[0] = sample_nums[0]
-        total_sample_nums[1:] = sample_nums[0] + sample_nums[1:]        
+        total_sample_nums[1:] = sample_nums[0] + sample_nums[1:]
         cost = np.dot(total_sample_nums, self._model_costs)
+        return cost
 
     def _get_cost_constraint(self, target_cost):
 
@@ -57,10 +66,6 @@ class ACVMF(OptimizerBase):
         return constraint_dict
 
     def _compute_objective_function(self, sample_nums, target_cost):
-
-        if self._num_models == 1:
-            return self._covariance[0,0]/sample_nums[0], \
-                    -self._covariance[0,0]*sample_nums[0]**-2
 
         sample_nums = torch.tensor(sample_nums, requires_grad=True)
         covariance = torch.tensor(self._covariance)
