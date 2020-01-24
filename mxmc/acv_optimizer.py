b0a6983b76663489f72333c86746a30196ff89bc
Geoffrey Bomarito <geoffrey.f.bomarito@nasa.gov>
2020-01-24 16:26:08 -0700

    reorg acv base, getting ready for further abstraction

--- mxmc/acv_optimizer.py
+++ mxmc/acv_optimizer.py
@@ -25,23 +25,17 @@ class ACVOptimizer(OptimizerBase, ACVConstraints):
         if self._num_models == 1:
             return self._get_monte_carlo_result(target_cost)
 
-        ratios = self._solve_opt_problem(target_cost )
+        ratios = self._solve_opt_problem(target_cost)
 
         sample_nums = self._compute_sample_nums_from_ratios(ratios,
                                                             target_cost)
         sample_nums = np.floor(sample_nums)
-        ratios = self._compute_ratios_from_sample_nums(sample_nums)
-        actual_cost = self._compute_total_cost(sample_nums)
 
-        variance = self._compute_objective_function(ratios, actual_cost,
-                                                    gradient=False)
+        variance = self._compute_variance_from_sample_nums(sample_nums)
+        actual_cost = self._compute_total_cost_from_sample_nums(sample_nums)
         allocation = self._make_allocation(sample_nums)
 
         return OptimizationResult(actual_cost, variance, allocation)
-
-    def _compute_total_cost(self, sample_nums):
-        cost = np.dot(sample_nums, self._model_costs)
-        return cost
 
     def _solve_opt_problem(self, target_cost):
         initial_guess = self._get_initial_guess()
@@ -68,13 +62,6 @@ class ACVOptimizer(OptimizerBase, ACVConstraints):
     def _get_bounds(self):
         return [(1 + 1e-12, np.inf)] * (self._num_models - 1)
 
-    def _get_constraints(self, target_cost):
-        constraints = self._constr_n_greater_than_1(target_cost)
-        nr_constraints = \
-            self._constr_ratios_result_in_samples_1_greater_than_n(target_cost)
-        constraints.extend(nr_constraints)
-        return constraints
-
     def _compute_objective_function(self, ratios, target_cost, gradient):
         ratios_tensor = torch.tensor(ratios, requires_grad=gradient,
                                      dtype=TORCHDTYPE)
@@ -90,20 +77,8 @@ class ACVOptimizer(OptimizerBase, ACVConstraints):
                   ratios_tensor.grad.detach().numpy())
         return result
 
-    @staticmethod
-    def _get_eval_ratios(ratios_tensor):
-        full_ratios = np.ones(len(ratios_tensor) + 1)
-        full_ratios[1:] = ratios_tensor
-        return full_ratios
-
-    @staticmethod
-    def _get_eval_ratios_autodiff(ratios_tensor):
-        full_ratios = torch.ones(len(ratios_tensor) + 1, dtype=TORCHDTYPE)
-        full_ratios[1:] = ratios_tensor
-        return full_ratios
-
     def _calculate_n(self, ratios, target_cost):
-        eval_ratios = self._get_eval_ratios(ratios)
+        eval_ratios = self._get_model_eval_ratios(ratios)
         N = target_cost / (np.dot(self._model_costs, eval_ratios))
         return N
 
@@ -124,15 +99,32 @@ class ACVOptimizer(OptimizerBase, ACVConstraints):
         variance = covariance[0, 0] / N * (1 - R_squared)
         return variance
 
-    @staticmethod
-    def _compute_ratios_from_sample_nums(sample_nums):
-        ratios = sample_nums[1:] / sample_nums[0]
-        return ratios
-
     def _compute_sample_nums_from_ratios(self, ratios, target_cost):
         N = self._calculate_n(ratios, target_cost)
         sample_nums = N * np.array([1] + list(ratios))
         return sample_nums
+
+    def _compute_variance_from_sample_nums(self, sample_nums):
+        N = sample_nums[0]
+        ratios = sample_nums[1:] / N
+        ratios_tensor = torch.tensor(ratios, dtype=TORCHDTYPE)
+        variance = self._compute_acv_estimator_variance(
+            self._covariance_tensor, ratios_tensor, N)
+        return variance.detach().numpy()
+
+    def _compute_total_cost_from_sample_nums(self, sample_nums):
+        N = sample_nums[0]
+        ratios = sample_nums[1:] / N
+        eval_samples_nums = N * self._get_model_eval_ratios(ratios)
+        cost = np.dot(eval_samples_nums, self._model_costs)
+        return cost
+
+    def _get_constraints(self, target_cost):
+        constraints = self._constr_n_greater_than_1(target_cost)
+        nr_constraints = \
+            self._constr_ratios_result_in_samples_1_greater_than_n(target_cost)
+        constraints.extend(nr_constraints)
+        return constraints
 
     @abstractmethod
     def _compute_acv_F_and_F0(self, ratios):
@@ -141,3 +133,15 @@ class ACVOptimizer(OptimizerBase, ACVConstraints):
     @abstractmethod
     def _make_allocation(self, sample_nums):
         raise NotImplementedError
+
+    @staticmethod
+    def _get_model_eval_ratios(ratios):
+        full_ratios = np.ones(len(ratios) + 1)
+        full_ratios[1:] = ratios
+        return full_ratios
+
+    @staticmethod
+    def _get_eval_ratios_autodiff(ratios_tensor):
+        full_ratios = torch.ones(len(ratios_tensor) + 1, dtype=TORCHDTYPE)
+        full_ratios[1:] = ratios_tensor
+        return full_ratios
