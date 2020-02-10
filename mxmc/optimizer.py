7bd4109ee14123fb4090f730efd7724a5538e627
jwarner308 <james.e.warner@nasa.gov>
2020-02-10 18:34:26 -0500

    Doc string for optimizer

--- mxmc/optimizer.py
+++ mxmc/optimizer.py
@@ -15,12 +15,35 @@ ALGORITHM_MAP = {"mfmc": MFMC, "mlmc": MLMC, "acvmfu": ACVMFU, "acvmf": ACVMF,
 
 
 class Optimizer:
-
+    '''
+    User interface for accessing all MXMC variance minimization optimizers for
+    computing optimal sample allocations.
+    '''
     def __init__(self, *args, **kwargs):
         self._args = args
         self._kwargs = kwargs
 
     def optimize(self, algorithm, target_cost, auto_model_selection=False):
+        '''
+        Performs variance minimization optimization to determine the optimal
+        sample allocation across available models within a specified target
+        computational cost.
+
+        :param algorithm: name of method to use for optimization (e.g., "mlmc",
+            "mfmc", "acvkl").
+        :type algorithm: string
+        :param target_cost: total target cost constraint so that optimizer finds
+            sample allocation that requires less than or equal computation time.
+        :type target_cost: float
+        :param auto_model_selection: flag to use automatic model selection in
+            optimization to test all subsets of models for best set.
+        :type auto_model_selection: Boolean
+
+        :Returns: An OptimizationResult namedtuple with entries for cost,
+            variance, and sample_array. cost (float) is expected cost of all 
+            model evaluations prescribed in sample_array (np.array). variance
+            is the minimized variance from the optimization.
+        '''
         optimizer = ALGORITHM_MAP[algorithm](*self._args, **self._kwargs)
         if auto_model_selection:
             optimizer = AutoModelSelection(optimizer)
