19efda71c3a061a76c6ada5eaed34946b7299326
Leser <pleser@ndc.nasa.gov>
2020-06-19 16:40:26 -0400

    reintroduced the k matrix calcs to ACVSampleAllocation to pass original tests; included what will likely be a merge conflict in estimator in order to get passing

--- mxmc/optimizers/optimization_result.py
+++ mxmc/optimizers/optimization_result.py
@@ -1,9 +1,12 @@
-from ..sample_allocation import SampleAllocation
+from ..sample_allocations.acv_sample_allocation import ACVSampleAllocation
+from ..sample_allocations.mlmc_sample_allocation import MLMCSampleAllocation
 
+ALLOC_MAP = {None: ACVSampleAllocation,    # noqa: F405
+             "mlmc": MLMCSampleAllocation} # noqa: F405
 
 class OptimizationResult:
 
     def __init__(self, cost, variance, sample_array, method=None):
         self.cost = cost
         self.variance = variance
-        self.allocation = SampleAllocation(sample_array, method)
+        self.allocation = ALLOC_MAP[method](sample_array)
--- mxmc/sample_allocations/mlmc_sample_allocation.py
+++ mxmc/sample_allocations/mlmc_sample_allocation.py
@@ -1,1 +1,7 @@
+from .sample_allocation_base import SampleAllocationBase
+
+
 class MLMCSampleAllocation(SampleAllocationBase):
+
+    def __init__(self, compressed_allocation):
+        pass
