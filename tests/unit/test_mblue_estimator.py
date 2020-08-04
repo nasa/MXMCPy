89bc1fcdbf534233aed6d6ca2a40710080eb051c
jwarner308 <james.e.warner@nasa.gov>
2020-08-04 16:32:53 -0400

    Added mblue estimator test for 3 models

--- tests/unit/test_mblue_estimator.py
+++ tests/unit/test_mblue_estimator.py
@@ -19,6 +19,23 @@ def sample_allocation_2models(compressed_allocation_2models):
                             category=UserWarning)
     return MBLUESampleAllocation(compressed_allocation_2models)
 
+@pytest.fixture
+def compressed_allocation_3models():
+    return np.array([[1, 1, 0, 0],
+                     [2, 0, 1, 0],
+                     [0, 0, 0, 1],
+                     [2, 1, 1, 0],
+                     [1, 1, 0, 1],
+                     [0, 0, 1, 1],
+                     [0, 1, 1, 1]])
+
+@pytest.fixture
+def sample_allocation_3models(compressed_allocation_3models):
+    warnings.filterwarnings("ignore",
+                            message="Allocation Warning",
+                            category=UserWarning)
+    return MBLUESampleAllocation(compressed_allocation_3models)
+
 
 def test_approximate_variance_two_models(sample_allocation_2models):
 
@@ -27,7 +44,7 @@ def test_approximate_variance_two_models(sample_allocation_2models):
 
     assert estimator.approximate_variance == pytest.approx(5 / 11)
     
-def test_get_estimate_two_models(sample_allocatin_2models):
+def test_get_estimate_two_models(sample_allocation_2models):
     
     covariance = np.array([[1., 1/2.], [1/2., 1.]])
     model_outputs = [np.ones(2)*2., np.ones(3)]
@@ -35,3 +52,32 @@ def test_get_estimate_two_models(sample_allocatin_2models):
     estimator = Estimator(sample_allocation_2models, covariance)
 
     assert estimator.get_estimate(model_outputs) == pytest.approx(2.)
+
+
+def test_approximate_variance_three_models(sample_allocation_3models):
+    '''
+    Psi inverse = array([[0.23333333, 0.06666667, 0.05833333],
+       [0.06666667, 0.23333333, 0.01666667],
+       [0.05833333, 0.01666667, 0.95208333]])  - variance is [0,0] entry
+    '''
+
+    covariance = np.array([[1., 1/2., 1/4.], 
+                           [1/2., 1., 1/2.],
+                           [1/4., 1/2., 1.]])
+    estimator = Estimator(sample_allocation_3models, covariance)
+    expected_variance = 0.23333333
+
+    assert estimator.approximate_variance == pytest.approx(expected_variance)
+    
+
+def test_get_estimate_three_models(sample_allocation_3models):
+    
+
+    covariance = np.array([[1., 1/2., 1/4.], 
+                           [1/2., 1., 1/2.],
+                           [1/4., 1/2., 1.]])
+    model_outputs = [np.ones(4)*2., np.ones(4), np.ones(1)*2.]
+    estimator = Estimator(sample_allocation_3models, covariance)
+
+    assert estimator.get_estimate(model_outputs) == pytest.approx(2.)
+
