857ace80d97f625fce451f846881c7e1c35c1512
Leser <pleser@ndc.nasa.gov>
2020-08-07 12:22:06 -0400

    hooked up new save and read functions for refactored allocation

--- tests/unit/test_sample_allocation_base.py
+++ tests/unit/test_sample_allocation_base.py
@@ -93,7 +93,7 @@ def test_get_total_number_of_samples(sample_allocation):
 
 def test_save_allocation(sample_allocation, saved_allocation_path):
     sample_allocation.save(saved_allocation_path)
-    h5 = h5py.File(saved_allocation_path)
+    h5 = h5py.File(saved_allocation_path, 'r')
     assert h5.attrs.get('Method') == SampleAllocationBase.__module__
 
 
